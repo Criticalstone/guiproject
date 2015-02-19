8ad610aca8012861386663fdef354eb5479353fc
Kevin Hoogendijk <kevinhoog@gmail.com>
2015-02-19 13:45:31 +0100

    Rethinks the view, now using one fxml doc for the categories

--- src/guiProject/Main.java
+++ src/guiProject/Main.java
@@ -4,6 +4,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,7 +19,18 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
         primaryStage.setTitle("iMat");
 
-        Scene scene = new Scene(root, 300, 275);
+        TabPane tabPane = new TabPane();
+        Tab tab1 = new Tab("T1"); tab1.setContent(new Rectangle(200, 200, Color.LIGHTGREEN));
+        Tab tab2 = new Tab("T2"); tab2.setContent(new Rectangle(200, 200, Color.LIGHTSTEELBLUE));
+        tabPane.getTabs().addAll(tab1, tab2);
+        tabPane.setMaxSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+
+        // layout the stage.
+        StackPane layout = new StackPane();
+        layout.getChildren().add(tabPane);
+        layout.setStyle("-fx-background-color: cornsilk; -fx-padding: 10;");
+
+        Scene scene = new Scene(layout);
         scene.getStylesheets().add("/res/sample.css");
 
         primaryStage.setScene(scene);
