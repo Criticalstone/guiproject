092e28a4e7b35ad7b7e610d47bc9f15d25599e7d
Martin Tran <martin_t_39@hotmail.com>
2015-02-25 11:32:19 +0100

    Adds Startmeny and RecipeView GUI to the project. Hotfixes to Banner GUI

--- src/guiProject/Banner.java
+++ src/guiProject/Banner.java
@@ -4,12 +4,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import se.chalmers.ait.dat215.project.ProductCategory;
 
 import java.io.IOException;
 
-public class Banner extends AnchorPane{
+public class Banner extends GridPane{
 
     public Banner() {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
