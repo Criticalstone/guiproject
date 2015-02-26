1b1f32881aa087d566c4b828562c9985e10c2445
Marvin043 <marvin.ztrubas@gmail.com>
2015-02-26 15:08:03 +0100

    Removed unused imports

--- src/guiProject/ControllerProductCardPane.java
+++ src/guiProject/ControllerProductCardPane.java
@@ -11,7 +11,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
