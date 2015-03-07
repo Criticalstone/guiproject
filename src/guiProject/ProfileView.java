3f949ca5a8e18ede5e0a13f130155257205e310b
YenL <jennifer.linder@live.com>
2015-03-07 13:07:17 +0100

    fixed so able to change password on profile

--- src/guiProject/ProfileView.java
+++ src/guiProject/ProfileView.java
@@ -10,8 +10,11 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+
+import javax.swing.Timer;
 
 /**
  * Created by kritt on 2015-03-03.
@@ -65,7 +68,20 @@ public class ProfileView extends GridPane{
     
     @FXML
     private Button buttonSave;
-
+    @FXML
+    private VBox changePasswordBox;
+    @FXML
+    private TextField currentPassword;
+    @FXML
+    private TextField newPassword;
+    @FXML
+    private TextField repeatPassword;
+    @FXML
+    private Label wrongMessage;
+    @FXML
+    private Label correctMessage;
+    
+    
     public ProfileView(UserProfile user) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
                 "fxml/ProfileView.fxml"));
@@ -79,6 +95,7 @@ public class ProfileView extends GridPane{
             throw new RuntimeException(exception);
         }
         this.profile = user;
+        correctMessage.setVisible(false);
         setupChoiceBoxes();
         loadInfo();
     }
@@ -168,5 +185,42 @@ public class ProfileView extends GridPane{
 	//        customer.setMobilePhoneNumber(textTel.getText());
 	//        customer.setEmail(textEmail.getText());
     	
+    }
+    
+    @FXML
+    public void ChangePasswordOnAction(){
+    	changePasswordBox.setVisible(true);
+    }
+
+    
+    @FXML
+    public void SavePasswordOnAction(){
+    	if(profile.getPassword().equals(currentPassword.getText()) && newPassword.getText().equals(repeatPassword.getText())&& repeatPassword.getText().length()!=0 ){ /*kontotslösenord jämförs med inskrivet nuvarande lösenord && nyttlösenord jämförs med upprepat lösenord(ser om samma)*/
+    		if(profile.getPassword().equals(newPassword.getText())){
+    			wrongMessage.setText("*Samma lösenord som innan");
+        		wrongMessage.setVisible(true);
+    		}else{
+    			correctMessage.setVisible(true);  //få bort texten?
+    			profile.setPassword(newPassword.getText());
+    			cleanBox();
+    			Utilities.SaveToFile(profile, null, profile.getUsername());       /*sparar ner det nya lösenordet. Rutan försvinner och en liten text kommer upp att det är genomfört(försvinner seda)*/
+    		}
+    	}else if(!profile.getPassword().equals(currentPassword.getText())){/*om kontolösenord och nuvarande lösenord ej överstämmer*/
+    		wrongMessage.setText("*Ej korrekt nuvarande lösenord");
+    		wrongMessage.setVisible(true);  /*sätter upp en label som står att det är ej korrekt nuvarande lösenord*/		
+    	}else if(repeatPassword.getText().length()==0 && newPassword.getText().length()==0){  /*något av de två sista fälten är tomma*/
+    		wrongMessage.setText("*Skriv in ett nytt lösenord");
+    		wrongMessage.setVisible(true); /*du måste skriva in ett nytt lösenord*/
+    	}else{
+    		wrongMessage.setText("*Nya lösenord ej samma"); /*sätter upp en label, nya lösenordet överstämmer inte*/
+    	}
+    }
+    
+    public void cleanBox(){
+    	changePasswordBox.setVisible(false);
+    	currentPassword.clear();
+    	newPassword.clear();
+    	repeatPassword.clear();
+    	wrongMessage.setVisible(false);
     }
 }
